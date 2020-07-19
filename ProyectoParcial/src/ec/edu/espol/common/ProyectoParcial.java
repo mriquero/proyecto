43ad4f21549be7b041400b9abde93617fa7013a3
Tommy Burgos <trburgos@espol.edu.ec>
2020-07-18 19:37:07 -0500

    Avance del main

--- ProyectoParcial/src/ec/edu/espol/common/ProyectoParcial.java
+++ ProyectoParcial/src/ec/edu/espol/common/ProyectoParcial.java
@@ -6,6 +6,7 @@
 package ec.edu.espol.common;
 
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -23,7 +24,52 @@ public class ProyectoParcial {
         System.out.println("1. Vendedor");
         System.out.println("2. Comprador");
         System.out.println("3. Salir");
+        System.out.println("Ingrese el número: ");
+        int opcion=sc.nextInt();
+        System.out.println(opcion);
         
-    }
+        switch (opcion){
+            case 1: 
+                
+                System.out.println("1. Registrar nuevo vendedor ");
+                System.out.println("2. Ingresar un nuevo vehiculo: ");
+                System.out.println("3. Aceptar oferta ");
+                System.out.println("4. Regresar ");
+                System.out.println("Ingrese el número: ");
+                int opcion2= sc.nextInt();
+                switch(opcion2){
+                    case 1:
+                        System.out.println("Registre los datos a continuación: ");
+                        System.out.println("Nombre: ");
+                        String name=sc.next();
+                        System.out.println("El nombre es "+ name);
+                        System.out.print("Apellido: ");
+                        String lName= sc.nextLine();
+                        System.out.println(lName);
+                        System.out.print("Corre Electronico: ");
+                        String correo= sc.nextLine();
+                        while(!(correo.contains("@")&&(correo.contains(".com") || correo.contains("ec") || correo.contains("es")))){
+                            System.out.print("Correo Invalido, Ingresa otro: ");
+                            String correoN=sc.nextLine();
+                            correo=correoN;
+                        }
+                        System.out.println("Organización: ");
+                        String org= sc.nextLine();
+
+                }
+                break;
+            case 2: System.out.print("Ingrese su usuario: ");
+                    String us=sc.nextLine();
+                    System.out.println("Ingrese su contrseña: ");
+                    
+                break;
+            case 3: System.out.print("Ingrese su usuario: ");
+                    String us2=sc.nextLine();
+                    System.out.println("Ingrese su contrseña: ");
+            case 4: 
+                break;
+            
+                
+        }
     
-}
+}}
